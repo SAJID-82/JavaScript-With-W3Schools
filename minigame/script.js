f9bc3f1a75d7f7387cbea65630ddac8dafb83dbe
SAJID-82 <themohammedsajid@gmail.com>
2025-12-01 14:47:29 +0530

    01-12-2025

--- minigame/script.js
+++
@@ -1,5 +0,0 @@
-let ball = document.getElementById("ball");
-let controller = document.getElementById("control");
-let leftBtn = document.getElementById("left"); 
-let rightBtn = document.getElementById("right"); 
-
